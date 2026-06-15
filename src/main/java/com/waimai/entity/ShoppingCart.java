84e41f8f41f9f9365db8a7d142498b757b9309f1
Hanabidaka <Hanabidaka@noreply.gitcode.com>
2026-06-15 16:31:35 +0800

    update: 更新文件 ShoppingCart.java 加个注释
    Signed-off-by: Hanabidaka <Hanabidaka@noreply.gitcode.com>

--- src/main/java/com/waimai/entity/ShoppingCart.java
+++ src/main/java/com/waimai/entity/ShoppingCart.java
@@ -12,22 +12,22 @@ import java.math.BigDecimal;
 @Entity
 @Table(name = "shopping_cart")
 public class ShoppingCart extends BaseEntity {
-
+    //用户ID
     @Column(nullable = false)
     private Long userId;
-
+    //菜品ID
     @Column(nullable = false)
     private Long dishId;
-
+    //餐厅ID
     @Column(nullable = false)
     private Long restaurantId;
-
+    //菜名快照
     @Column(length = 100)
     private String dishName;
-
+    //菜价快照
     @Column(nullable = false, precision = 10, scale = 2)
     private BigDecimal dishPrice;
-
+    //数量
     @Column(nullable = false)
     private Integer quantity;
 }
