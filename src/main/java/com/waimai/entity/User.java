1af3a64451f79d6d90c845bc06e793b4603232b6
Hanabidaka <Hanabidaka@noreply.gitcode.com>
2026-06-15 16:36:21 +0800

    update: 更新文件 User.java 补充注释
    Signed-off-by: Hanabidaka <Hanabidaka@noreply.gitcode.com>

--- src/main/java/com/waimai/entity/User.java
+++ src/main/java/com/waimai/entity/User.java
@@ -11,26 +11,26 @@ import lombok.Setter;
 @Entity
 @Table(name = "users")
 public class User extends BaseEntity {
-
+    //用户名称
     @Column(nullable = false, unique = true, length = 50)
     private String username;
-
+    //密码
     @Column(nullable = false)
     private String password;
-
+    //昵称
     @Column(length = 100)
     private String nickname;
-
+    //电话
     @Column(length = 20)
     private String phone;
-
+    //展示图片地址
     @Column(length = 200)
     private String avatar;
-
+    //角色
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 20)
     private UserRole role = UserRole.CUSTOMER;
-
+    //地址
     @Column(length = 500)
     private String address;
 }
